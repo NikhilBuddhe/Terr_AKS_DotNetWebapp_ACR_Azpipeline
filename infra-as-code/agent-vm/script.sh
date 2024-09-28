24cc75ddbe0da7163344054926d78819d69f3a00
niks <niks123@gmail.com>
2024-09-28 12:18:32 +0530

    removing secret

--- infra-as-code/agent-vm/script.sh
+++ infra-as-code/agent-vm/script.sh
@@ -33,7 +33,7 @@ tar zxvf vsts-agent-linux-x64.tar.gz -C myagent
 chmod -R 777 myagent
 # Configuration of the self-hosted agent
 cd myagent
-./config.sh --unattended --url https://dev.azure.com/Learnazure162022 --auth pat --token pibvctgpmeutdwsy7vfypwercnf5ixknxgtwpcbiqn6eg5mlrchq --pool Default --agent aksagent --acceptTeeEula
+./config.sh --unattended --url https://dev.azure.com/Learnazure162022 --auth pat --token  --pool Default --agent aksagent --acceptTeeEula
 # Start the agent service
 sudo ./svc.sh install
 sudo ./svc.sh start
